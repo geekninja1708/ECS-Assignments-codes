5ae5ce502080e6daf7e07880cd3aa21772c4b674
geekninja1708 <68696719+geekninja1708@users.noreply.github.com>
2020-10-22 13:37:32 -0400

    Update ECS.py

--- ECS.py
+++ ECS.py
@@ -12,13 +12,11 @@ for u in P:
     m = np.random.random()
     for n in range(1001):
       m=(u*m)*(1-m)
-    # The break is harmful here as it prevents completion of
-    # the loop and collection of data in Y 
+     
     for l in range(1051):
       C
-    # Collection of data in Y must be done once per value of u
+    
     Y.append(m)
-# Remove the line between successive data points, this renders
-# the plot illegible. Use a small marker instead.
+
 plt.plot(X, Y, ls='', marker=',')
 plt.show()
